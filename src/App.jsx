6f7718684849b269bbc494e84f8508ee4e3b29d8
juellecaldwell1 <juelle_caldwell@insideranken.org>
2024-09-17 11:24:21 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 export default function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS! goku is next</h1>
     </div>
   );
 }
